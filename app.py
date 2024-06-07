02cd4e0d35e7f13d00845f7a7a20fac678c45c41
AmitabhAnand04 <98702241+AmitabhAnand04@users.noreply.github.com>
2024-06-07 17:14:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,5 @@ def incoming_sms():
 def hello_world():
     return 'Hello, World! This is my Flask app.'
     
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
